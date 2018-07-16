45c90258454acaac4dc60e1e6ffc2cef34e6d824
ckc1373 <ccourtne@hamilton.edu>
2018-07-16 14:46:50 -0700

    moved textbox to components, add color dropdown, with option for color picker if it can be implemented in the future

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TextBox from './components/TextBox';
 
 export default class App extends React.Component {
   render() {
     return (<div>
       <h2>Welcome to React!</h2>
+      <TextBox />
     </div>);
   }
 }
