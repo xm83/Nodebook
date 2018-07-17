d9a03b8e7fb5ae1086a394b04986c811ef9dc269
Krish Patel <kkpatel@bu.edu>
2018-07-17 16:03:46 -0700

    Names Showing

--- src/components/DocCard.jsx
+++ src/components/DocCard.jsx
@@ -10,16 +10,25 @@ class DocCard extends React.Component {
 
     this.state = {
       ownerName: "",
-      collaboratorNames: [],
+      collaboratorNames: "",
     }
   }
 
   componentDidMount() {
     axios.get(`http://localhost:1337/getAllEditors/` + this.props.doc._id)
     .then((res) => {
+      let collabNames = []
+      let collabs = res.data.project.collaborators.slice()
+      for (var x = 0; x < collabs.length; x++) {
+        let newName = collabs[x].firstName + ' ' + collabs[x].lastName
+        collabNames.push(newName)
+      }
+      console.log("line by line?")
       this.setState({
         ownerName: res.data.project.owner.firstName + ' ' + res.data.project.owner.lastName,
-      })      
+        collaboratorNames: collabNames
+      })
+      console.log(this.state.collaboratorNames)
       console.log(res.data)
     })
   }
@@ -29,7 +38,7 @@ class DocCard extends React.Component {
       <div>
         <p>{this.props.doc.title}</p>
         <p> Owner: {this.state.ownerName} </p>
-        <p> Collaboraters: {this.props.doc.collaborators} </p>
+        <p> Collaboraters: {this.state.collaboratorNames} </p>
         <Button type="Open" onClick={()=>this.props.openDoc()} />
       </div>
     )
