15129cd1219065456a061365efab07c4194becc6
ckc1373 <ccourtne@hamilton.edu>
2018-07-16 17:00:41 -0700

    Font size working, align is close to working

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Editor, EditorState, RichUtils, Modifier } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import { HuePicker } from 'react-color';
 
 
-const styleMap = {};
+const styleMap = {
+  right: {
+    textAlign: 'right',
+  },
+  center: {
+    textAlign: 'center',
+  },
+  left: {
+    textAlign: 'left',
+  },
+};
 
 export default class TextBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { editorState: EditorState.createEmpty(), color: '#fff' };
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      color: '#fff',
+      fontInput: 0,
+      align: 'left',
+    };
     this.onChange = editorState => this.setState({ editorState });
   }
   bold() {
@@ -44,6 +59,20 @@ export default class TextBox extends React.Component {
     ));
   }
 
+  font(fontSize) {
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      fontSize,
+    ));
+  }
+
+  align(side) {
+    console.log(styleMap[side]);
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      side,
+    ));
+  }
 
   render() {
     return (
@@ -52,7 +81,21 @@ export default class TextBox extends React.Component {
           <button onClick={() => { this.bold(); }}><b>B</b></button>
           <button onClick={() => { this.italicize(); }}><i>I</i></button>
           <button onClick={() => { this.underline(); }}><u>U</u></button>
-          <button onClick={() => { this.code(); }}>Code</button>
+          <button onClick={() => { this.code(); }}>Code</button><br />
+          <input
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                styleMap[String(e.target.value)] = { fontSize: e.target.value };
+                this.font(String(e.target.value));
+              }
+            }
+                  }
+            onChange={(e) => {
+              this.setState({ fontInput: e.target.value });
+            }}
+            type="number"
+            value={this.state.fontInput}
+          />
           <HuePicker
             color={this.state.color}
             onChangeComplete={(color) => {
@@ -61,9 +104,16 @@ export default class TextBox extends React.Component {
               this.toggleColor(String(color.hex));
             }}
           />
+          <button onClick={() => { this.align('left'); }}>Left</button>
+          <button onClick={() => { this.align('center'); }}>Center</button>
+          <button onClick={() => { this.align('right'); }}>Right</button><br/>
+          <button onClick={() => { this.setState({ align: 'left' }); }}> All Left </button>
+          <button onClick={() => { this.setState({ align: 'center' }); }}> All Center </button>
+          <button onClick={() => { this.setState({ align: 'right' }); }}> All Right </button>
         </div>
         <div className="editor">
           <Editor
+            textAlignment={this.state.align}
             customStyleMap={styleMap}
             editorState={this.state.editorState}
             onChange={this.onChange}
