d3053867aeab3a6fc7211505432ff98140a5cf30
Nicholas_Marwell <nmarwell@gmail.com>
2018-07-19 14:55:27 -0700

    animations on mainhub including border color and cursor change when hovering over doc

--- src/components/DocCard.jsx
+++ src/components/DocCard.jsx
@@ -14,9 +14,9 @@ class DocCard extends React.Component {
   render() {
     return (
       <Draggable type="document" onMouseDown={()=>this.props.sendData()}>
-        <div onClick={()=>this.props.openDoc()} className="text-center d-inline-block"
+        <div onClick={()=>this.props.openDoc()} className=" doc-card text-center d-inline-block"
             style={{marginTop: '3vh', marginLeft: '1vw',
-            marginRight: '1vw', border: '2px solid black', background: 'white', height: '35vh', width: '15vw'}}>
+            marginRight: '1vw', background: 'white', height: '35vh', width: '15vw'}}>
           <div style={{background: 'linear-gradient(to top, #dfe9f3 0%, white 100%)', display: 'flex', flex: '1', flexDirection: 'column', height: '100%'}}>
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '30px', flex: '2'}}>
               <div>{this.props.doc.title}</div>
@@ -24,10 +24,6 @@ class DocCard extends React.Component {
             <div style={{fontSize: '12px', flex: '1'}}>
               <div> {this.props.doc.owner.firstName} {this.props.doc.owner.lastName} </div>
               <div>{new Date(this.props.doc.createdAt).toLocaleDateString()}</div>
-              {/* <button style={{color: 'grey'}} type="button" className="btn btn-sm btn-link button" onClick={(e)=>{
-                e.stopPropagation()
-                this.props.deleteDoc()
-              }}>Delete</button> */}
             </div>
           </div>
         </div>
